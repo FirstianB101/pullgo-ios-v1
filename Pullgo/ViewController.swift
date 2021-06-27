8fbe008946058b0add989c2c4afb72e6f58e4656
김세영 <yy0867@gmail.com>
2021-06-27 20:42:03 +0900

    init

---
+++ Pullgo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pullgo
+//
+//  Created by 김세영 on 2021/06/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
