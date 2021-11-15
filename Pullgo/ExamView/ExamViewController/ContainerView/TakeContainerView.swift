33699c4938c1c1641e86d5e2b96f983a81fc2fe4
김세영 <yy0867@gmail.com>
2021-11-16 01:50:27 +0900

    Academy 생성 및 Question Create UI 구현 완료

---
+++ Pullgo/ExamView/ExamViewController/ContainerView/TakeContainerView.swift
@@ -0,0 +1,20 @@
+//
+//  HistoryContainerView.swift
+//  Pullgo
+//
+//  Created by 김세영 on 2021/11/10.
+//
+
+import UIKit
+
+class HistoryContainerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Pullgo/Utility/TestUtility.swift
@@ -0,0 +1,8 @@
+//
+//  TestUtility.swift
+//  Pullgo
+//
+//  Created by 김세영 on 2021/11/12.
+//
+
+import Foundation
