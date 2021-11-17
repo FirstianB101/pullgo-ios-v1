9f0f8d55c5c6d90b3aad3eb384784f078eb4e814
김세영 <yy0867@gmail.com>
2021-11-18 02:57:09 +0900

    및  기능 구현 완료, 이미지 제어 완료

--- Pullgo/ExamView/ExamViewController/ChoiceView/TakeExamChoiceView.swift
+++ Pullgo/ExamView/ExamViewController/ChoiceView/TakeExamChoiceView.swift
@@ -9,5 +9,21 @@ import UIKit
 
 class TakeExamChoiceView: ChoiceView {
 
-
+    override init(examType: ExamType, viewModel: ExamViewModel) {
+        super.init(examType: examType, viewModel: viewModel)
+        
+        appendChoice()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func appendChoice() {
+        for i in 1 ... 5 {
+            let choice = PGChoiceView(number: i, state: .normal, examType: examType, viewModel: viewModel)
+            
+            super.appendChoice(choice)
+        }
+    }
 }
